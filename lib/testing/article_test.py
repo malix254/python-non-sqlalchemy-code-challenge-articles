200351fadf49885fb61730ed331e4e41e50f2738
malix254 <griffinskirimimbae@gmail.com>
2024-06-02 21:29:53 +0300

    done

--- lib/testing/article_test.py
+++ lib/testing/article_test.py
@@ -1,8 +1,5 @@
 import pytest
-
-from classes.many_to_many import Article
-from classes.many_to_many import Magazine
-from classes.many_to_many import Author
+from classes.many_to_many import Article, Magazine, Author
 
 
 class TestArticle:
@@ -24,15 +21,10 @@ class TestArticle:
         magazine = Magazine("Vogue", "Fashion")
         article_1 = Article(author, magazine, "How to wear a tutu with style")
 
-        # comment out the next two lines if using Exceptions
-        article_1.title = 500
-        assert article_1.title == "How to wear a tutu with style"
-        
-        assert isinstance(article_1.title, str)
+        with pytest.raises(AttributeError):
+            article_1.title = 500
 
-        # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     Article(author, magazine, 500)
+        assert isinstance(article_1.title, str)
 
     def test_title_is_valid(self):
         """title is between 5 and 50 characters inclusive"""
@@ -42,13 +34,11 @@ class TestArticle:
 
         assert 5 <= len(article_1.title) <= 50
 
-        # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     Article(author, magazine, "Test")
+        with pytest.raises(ValueError):
+            Article(author, magazine, "Test")
 
-        # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     Article(author, magazine, "How to wear a tutu with style and walk confidently down the street")
+        with pytest.raises(ValueError):
+            Article(author, magazine, "How to wear a tutu with style and walk confidently down the street")
 
     def test_has_an_author(self):
         """article has an author"""
